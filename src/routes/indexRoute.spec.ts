046faeaaf3518bceff944a489abc4edb2d6fe4c0
Gizachew Bayness <83591565+eleccrazy@users.noreply.github.com>
2023-11-14 19:45:31 +0000

    Adding unittest test for the /students route and adding some more studntes to the response body of the /students route

--- src/routes/indexRoute.spec.ts
+++ src/routes/indexRoute.spec.ts
@@ -14,6 +14,12 @@ describe('Test for index router', () => {
     expect(response.status).toBe(200);
   })
 
+  // Test if the /students route returns an array with length 10.
+  it('the /students route should return an array with length 10', async () => {
+    const response = await request.get('/api/students');
+    expect(response.body.length).toBe(10);
+  })
+
    // Test if the /departments route responds with a 200 ok status code
    it('the /departments route should respond with a 200 ok status code', async () => {
     const response = await request.get('/api/departments');
--- src/routes/indexRoute.ts
+++ src/routes/indexRoute.ts
@@ -2,20 +2,26 @@ import { Router, Request, Response } from 'express';
 
 const indexRouter = Router();
 
+const students = [
+  { id: 1, firstName: 'John', lastName: 'Doe', age: 20, gender: 'Male' },
+  { id: 2, firstName: 'Jane', lastName: 'Smith', age: 19, gender: 'Female' },
+  { id: 3, firstName: 'Michael', lastName: 'Johnson', age: 21, gender: 'Male' },
+  { id: 4, firstName: 'Emily', lastName: 'Brown', age: 20, gender: 'Female' },
+  { id: 5, firstName: 'David', lastName: 'Davis', age: 22, gender: 'Male' },
+  { id: 6, firstName: 'Emma', lastName: 'Wilson', age: 19, gender: 'Female' },
+  { id: 7, firstName: 'Daniel', lastName: 'Taylor', age: 20, gender: 'Male' },
+  { id: 8, firstName: 'Olivia', lastName: 'Thomas', age: 19, gender: 'Female' },
+  { id: 9, firstName: 'Sophia', lastName: 'Anderson', age: 21, gender: 'Female' },
+  { id: 10, firstName: 'James', lastName: 'Miller', age: 22, gender: 'Male' }
+];
+
 indexRouter.get('/', (req: Request, res: Response) => {
     res.send('Welcome to student information api!');
   });
 
 
   indexRouter.get('/students', (req: Request, res: Response) => {
-    res.send([
-      {
-        firstName: 'Gizachew',
-        lastName: 'Bayness',
-        gender: 'Male',
-        department: 'Çomputer Science'
-      }
-    ])
+    res.send(students)
   })
 
   indexRouter.get('/departments', (req: Request, res: Response) => {
